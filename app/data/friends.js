8c941d922dc420e732d8c922a8702337137deb61
LiftoffSBC <eagillet@gmail.com>
2018-07-11 12:37:38 -0400

    updating current work

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+// Data
+//// Below data will hold all of the possible friend data.
+
+var friendArray = [
+    {
+      friendName: "Ahmed",
+      friendPhoto: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      friendScores: [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+    }
+  ];
+  
+  // Export the array.
+  module.exports = friendArray;
+  
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,17 @@
+// LOAD DATA
+var friendData = require("../data/friends");
+
+// ROUTING
+module.exports = function(app) {
+  // API GET Requests
+  app.get("/api/friends", function(req, res) {
+    res.json(friendData);
+  });
+
+  // API POST Requests
+  app.post("/api/friends", function(req, res) {
+      res.json(friendData);
+  });
+
+    console.log(friendData);
+};
